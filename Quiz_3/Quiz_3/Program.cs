9c0a993e3ec0f84085b135d850780549ef5a2c46
tagnut <93494310+tagnut@users.noreply.github.com>
2021-10-31 17:52:15 -0400

    Quiz_3
    Quiz_3

---
+++ Quiz_3/Quiz_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Quiz_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] MarvelHero = { "아이언맨", "토르", "헐크" };
+            int MaxMarvelHero = 3;
+            for(int i=0; i<MaxMarvelHero; i++)
+            {
+                Console.WriteLine(MarvelHero[i]);
+            }
+
+        }
+    }
+}
