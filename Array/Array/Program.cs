08d6f871757ee96fd5ba2e982cadfc5d535cbfb3
tagnut <93494310+tagnut@users.noreply.github.com>
2021-10-31 17:50:56 -0400

    Array
    Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Singer = { "김범수", "나얼", "박효신", "박정현", "이수","" };
+
+            int MaxSinger = 5;
+            /*         
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(Singer[i]);
+            }
+            */
+            for (int i = 0; i <= MaxSinger; i++)
+            {
+                Console.WriteLine(Singer[i]);
+            }
+        }
+    }
+}
