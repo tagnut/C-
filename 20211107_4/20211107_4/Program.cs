0fd1a88155d12e79c06bb8cd40ce016000a62a3b
tagnut <93494310+tagnut@users.noreply.github.com>
2021-11-07 18:02:31 -0500

    test
    1234

---
+++ 20211107_4/20211107_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _20211107_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+
+            Console.WriteLine("당신의 차의 연식은?");
+            result = Console.ReadLine();
+            //swtich 예제
+            switch (result)
+            {
+                case "2016":
+                    Console.WriteLine("운행가능합니다.");
+                    break;
+                case "2017":
+                    Console.WriteLine("운행가능합니다.");
+                    break;
+                case "2018":
+                    Console.WriteLine("운행가능합니다.");
+                    break;
+                case "2019":
+                    Console.WriteLine("운행가능합니다.");
+                    break;
+                case "2020":
+                    Console.WriteLine("운행가능합니다.");
+                    break;
+                default:
+                    Console.WriteLine("버리세요.");
+                    break;
+
+            }
+        }
+    }
+}
